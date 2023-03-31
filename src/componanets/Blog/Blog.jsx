fe0501b0dac154a87d1bb4505f68d16c24adfd91
webashraf <aliashraftamim@gmail.com>
2023-03-31 12:29:49 +0600

    add user in blog card

--- src/componanets/Blog/Blog.jsx
+++ src/componanets/Blog/Blog.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const Blog = (props) => {
-          console.log(props);
-          const {cover_photo} = props.blog
-          return (
-             <div>
-               <div className="card w-[90%] mx-auto mt-8 bg-base-100 shadow-xl">
-              <figure><img src={cover_photo} alt="Shoes" /></figure>
-                <div className="card-body">
-                <h2 className="card-title">Shoes</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Buy Now</button>
-                </div>
-              </div>
+  console.log(props);
+  const { cover_photo, title, user_pic, user_name, publish_date } = props.blog;
+  return (
+    <div>
+      <div className="card w-[90%] mx-auto mt-8 bg-base-100 shadow-xl">
+        <figure>
+          <img src={cover_photo} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <div className="user flex gap-3 items-center">
+            <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
+              <img className="w-full" src={user_pic} alt="" />
+            </div>
+            <div className="user-detils text-left">
+              <span className="font-bold">{user_name}</span>
+              <br />
+              <span>
+                <small className="font-bold text-gray-600">
+                  {publish_date}
+                </small>
+              </span>
             </div>
           </div>
-          );
+
+          <h2 className="card-title">{title}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/componanets/Header/Header.jsx
+++ src/componanets/Header/Header.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-          return (
-                    <>
-                            <div className='flex justify-between'>
-                    <h2 className="logo">Programming Art</h2>
-                    <div className="header-menu">
-                              <ul className='flex gap-5 items-center'>
-                                        <li><a href="#">Home</a></li>
-                                        <li><a href="#">Blog</a></li>
-                                        <li><a href="#">Login</a></li>
-                                        <span className='w-[40px] rounded-full overflow-hidden'><a href="#"><img className='w-full' src="https://images.pexels.com/photos/10311994/pexels-photo-10311994.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /></a></span>
-                              </ul>
-                    </div>
-
-                            </div>
-                            <hr />
-                    </>
-          );
+  return (
+    <>
+      <div className="flex justify-between">
+        <h2 className="logo">Programming Art</h2>
+        <div className="header-menu">
+          <ul className="flex gap-5 items-center">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+            <span className="w-[40px] rounded-full overflow-hidden">
+              <a href="#">
+                <img
+                  className="w-full"
+                  src="https://images.pexels.com/photos/10311994/pexels-photo-10311994.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+              </a>
+            </span>
+          </ul>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
 };
 
 export default Header;
